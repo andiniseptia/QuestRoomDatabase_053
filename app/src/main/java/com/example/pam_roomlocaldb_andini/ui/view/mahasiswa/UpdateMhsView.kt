152ce64f169918c3da49d1a2856717cba7c51f50
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-16 14:59:20 +0700

    Menambahkan fungsi UpdateMhsView dengan penanganan snackbar dan integrasi ViewModel

---
+++ app/src/main/java/com/example/pam_roomlocaldb_andini/ui/view/mahasiswa/UpdateMhsView.kt
@@ -0,0 +1,53 @@
+package com.example.pam_roomlocaldb_andini.ui.view.mahasiswa
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_roomlocaldb_andini.ui.customwidget.TopAppBar
+import com.example.pam_roomlocaldb_andini.ui.viewmodel.PenyediaViewModel
+import com.example.pam_roomlocaldb_andini.ui.viewmodel.UpdateMhsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@Composable
+fun UpdateMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory) // Inisialisasi ViewModel
+) {
+    val uiState = viewModel.updateUIState // Ambil UI state dari ViewModel
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    val coroutineScope = rememberCoroutineScope()
+
+    // Observasi perubahan snackbarMessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        println("LaunchedEffect triggered")
+        uiState.snackBarMessage?.let { message ->
+            println("Snackbar message received : $message")
+            coroutineScope.launch {
+                println("Launching corroutine for snackbar")
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+
+}
