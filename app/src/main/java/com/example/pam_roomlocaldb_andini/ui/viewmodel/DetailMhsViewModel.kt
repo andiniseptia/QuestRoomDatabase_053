3595fc495ffe92c7972e7c779d3655b6086484a8
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-16 15:11:59 +0700

    Menambahkan data class DetailUiState untuk mengelola state UI detail mahasiswa dengan status loading dan error.

--- app/src/main/java/com/example/pam_roomlocaldb_andini/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pam_roomlocaldb_andini/ui/viewmodel/DetailMhsViewModel.kt
@@ -20,6 +20,20 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
 // Data class untuk menampung data yang akan ditampilkan di UI
 
 // Memindahkan data dari entity ke ui
