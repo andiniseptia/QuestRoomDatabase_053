230a5aaffeffdb718d533f042d4daa08d8b4a6a2
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-16 14:24:07 +0700

    Menambahkan objek DestinasiHome yang mengimplementasikan AlamatNavigasi untuk navigasi ke halaman home.

--- app/src/main/java/com/example/pam_roomlocaldb_andini/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pam_roomlocaldb_andini/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,8 @@ package com.example.pam_roomlocaldb_andini.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
