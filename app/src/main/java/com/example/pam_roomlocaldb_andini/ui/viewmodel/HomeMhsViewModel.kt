3e9587ee5daedaadd95769845ab7268bcecef94d
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-16 15:06:57 +0700

    Menambahkan HomeMhsViewModel untuk mengelola state UI mahasiswa dengan penanganan loading, error, dan data dari repository.

--- app/src/main/java/com/example/pam_roomlocaldb_andini/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pam_roomlocaldb_andini/ui/viewmodel/HomeMhsViewModel.kt
@@ -13,7 +13,39 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
 
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState (
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeUiState (
     val listMhs: List<Mahasiswa> = listOf(),
