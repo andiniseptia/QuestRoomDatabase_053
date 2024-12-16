90b652637d626e23b073ce978009940c9905693d
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-16 14:22:27 +0700

    Menambahkan fungsi getAllMhs, getMhs, deleteMhs, dan updateMhs di RepositoryMhs.

--- app/src/main/java/com/example/pam_roomlocaldb_andini/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pam_roomlocaldb_andini/repository/RepositoryMhs.kt
@@ -4,7 +4,18 @@ import android.content.Context
 import com.example.pam_roomlocaldb_andini.data.database.KrsDatabase
 import com.example.pam_roomlocaldb_andini.data.entity.Mahasiswa
 import com.example.pam_roomlocaldb_andini.dependenciesinjection.InterfaceContainerApp
+import kotlinx.coroutines.flow.Flow
 
+// Blueprint yang merepresentasikan dari DAO
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    // Sama persis seperti yang ada di MahasiswaDao
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
