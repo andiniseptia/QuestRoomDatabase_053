e7b6413f3acbc32b42b074e8feff4cf62afc212e
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-16 15:17:21 +0700

    Menambahkan initializer untuk menyediakan instansi HomeMhsViewModel, DetailMhsViewModel, dan UpdateMhsViewModel dengan akses ke repositoryMhs melalui KrsApp.

--- app/src/main/java/com/example/pam_roomlocaldb_andini/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam_roomlocaldb_andini/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.pam_roomlocaldb_andini.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -15,8 +16,24 @@ object PenyediaViewModel {
                 krsApp().containerApp.repositoryMhs
             )
         }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
     }
 }
 
-fun CreationExtras.krsApp(): KrsApp =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
+fun CreationExtras.krsApp(): KrsApp = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
