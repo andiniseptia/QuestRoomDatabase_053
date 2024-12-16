e564711f5ccef1ddae0d2f4790b48c0fc4c2bb11
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-16 14:13:11 +0700

    Menambahkan MahasiswaDao dengan metode insert dan query untuk mengambil data Mahasiswa berurutan dari nama.

--- app/src/main/java/com/example/pam_roomlocaldb_andini/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pam_roomlocaldb_andini/data/dao/MahasiswaDao.kt
@@ -1,19 +1,22 @@
 package com.example.pam_roomlocaldb_andini.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import com.example.pam_roomlocaldb_andini.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
-    suspend fun insertMahasiswa(
-        mahasiswa: Mahasiswa
-    )
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 
-    @Update
-    suspend fun updateMahasiswa(
-        mahasiswa: Mahasiswa
-    )
+    //Query untuk mendapat seluruh data mahasiswa yang diurutkan dari A-Z, A = Ascending
+    //Banyak data menggunakan List
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    //Untuk membungkus data
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
 }
